4dac733e1c78c08a045a1e7dd9bcae42e530895b
Vitor Camilo <vittor.camilo@gmail.com>
2025-11-05 01:03:01 -0300

    Get products paged

--- exercício/src/main/java/com/vitorcamilodev/exercicio/services/ProductService.java
+++ exercício/src/main/java/com/vitorcamilodev/exercicio/services/ProductService.java
@@ -1,10 +1,15 @@
 package com.vitorcamilodev.exercicio.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vitorcamilodev.exercicio.dto.ProductDTO;
+import com.vitorcamilodev.exercicio.entities.Product;
 import com.vitorcamilodev.exercicio.repositories.ProductRepository;
 
 @Service
@@ -16,6 +21,12 @@ public class ProductService {
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		return new ProductDTO(productRepository.findById(id).get());
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = productRepository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
 	}
 
 }
