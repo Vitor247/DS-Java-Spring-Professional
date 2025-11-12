c1e0251104ae800dae39dc11e414c507e8c9a42d
Vitor Camilo <vittor.camilo@gmail.com>
2025-11-12 01:37:54 -0300

    URI 2621 Spring Boot SQL e JPQL

--- bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -11,9 +11,13 @@ import com.devsuperior.uri2602.projections.CustomerMinProjection;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-	@Query(nativeQuery = true, value = "SELECT name" + " FROM customers " + " WHERE UPPER(state) = UPPER(:state)")
+	@Query(nativeQuery = true, value = "SELECT name"
+			+ " FROM customers"
+			+ " WHERE UPPER(state) = UPPER(:state)")
 	List<CustomerMinProjection> search1(String state);
 
-	@Query("SELECT new com.devsuperior.uri2602.dto.CustomerMinDTO(obj.name)" + " FROM Customer obj" + " WHERE UPPER(obj.state) = UPPER(:state)")
+	@Query("SELECT new com.devsuperior.uri2602.dto.CustomerMinDTO(obj.name)"
+			+ " FROM Customer obj"
+			+ " WHERE UPPER(obj.state) = UPPER(:state)")
 	List<CustomerMinDTO> search2(String state);
 }
--- bdsconsultas-main/uri2611/src/main/java/com/devsuperior/uri2611/repositories/MovieRepository.java
+++ bdsconsultas-main/uri2611/src/main/java/com/devsuperior/uri2611/repositories/MovieRepository.java
@@ -11,9 +11,14 @@ import com.devsuperior.uri2611.projections.MovieMinProjection;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-	@Query(nativeQuery = true, value = "SELECT movies.id, movies.name FROM movies INNER JOIN genres ON movies.id_genres = genres.id WHERE genres.description = :genreName")
+	@Query(nativeQuery = true, value = "SELECT movies.id, movies.name"
+			+ " FROM movies"
+			+ " INNER JOIN genres ON movies.id_genres = genres.id"
+			+ " WHERE genres.description = :genreName")
 	List<MovieMinProjection> search1(String genreName);
 
-	@Query("SELECT new com.devsuperior.uri2611.dto.MovieMinDTO(obj.id, obj.name) FROM Movie obj WHERE obj.genre.description = :genreName")
+	@Query("SELECT new com.devsuperior.uri2611.dto.MovieMinDTO(obj.id, obj.name)"
+			+ " FROM Movie obj"
+			+ " WHERE obj.genre.description = :genreName")
 	List<MovieMinDTO> search2(String genreName);
 }
