40599f1f9886a4aa25aafd08f9e97496ef1c52d6
Vitor Camilo <vittor.camilo@gmail.com>
2025-11-12 21:38:16 -0300

    Consulta de produtos por nome

---
+++ exercício - commerce/src/main/java/com/vitorcamilodev/commerce/dto/ProductDTO.java
@@ -0,0 +1,86 @@
+package com.vitorcamilodev.commerce.dto;
+
+import com.vitorcamilodev.commerce.entities.Product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public class ProductDTO {
+
+	private Long id;
+
+	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+	@NotBlank(message = "Campo requerido")
+	private String name;
+
+	@Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+	@NotBlank(message = "Campo requerido")
+	private String description;
+
+	@Positive(message = "O preço deve ser positivo")
+	private Double price;
+
+	private String imgUrl;
+
+	public ProductDTO() {
+
+	}
+
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		name = entity.getName();
+		description = entity.getDescription();
+		price = entity.getPrice();
+		imgUrl = entity.getImgUrl();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+}
