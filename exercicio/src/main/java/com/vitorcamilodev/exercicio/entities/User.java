0f62609565b2c8ccf5e0c700bcd8976eee123eaa
Vitor Camilo <vittor.camilo@gmail.com>
2025-11-18 02:44:08 -0300

    Security, OAuth2 JWT, route access control

--- exercicio/src/main/java/com/vitorcamilodev/exercicio/entities/User.java
+++ exercicio/src/main/java/com/vitorcamilodev/exercicio/entities/User.java
@@ -1,32 +1,43 @@
 package com.vitorcamilodev.exercicio.entities;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails{
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
 	private String name;
 
 	@Column(unique = true)
 	private String email;
-
 	private String password;
 
-	public User() {
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
+	public User() {
 	}
 
 	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
@@ -68,6 +79,19 @@ public class User {
 		this.password = password;
 	}
 
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o)
@@ -85,4 +109,13 @@ public class User {
 		return id != null ? id.hashCode() : 0;
 	}
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
 }
