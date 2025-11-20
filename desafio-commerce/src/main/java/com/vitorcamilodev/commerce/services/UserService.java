12ce1bac39c4b247467b8fdd5f257f44569abfa4
Vitor Camilo <vittor.camilo@gmail.com>
2025-11-20 13:48:02 -0300

    User controller, GET user logged

--- desafio-commerce/src/main/java/com/vitorcamilodev/commerce/services/UserService.java
+++ desafio-commerce/src/main/java/com/vitorcamilodev/commerce/services/UserService.java
@@ -3,36 +3,58 @@ package com.vitorcamilodev.commerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.vitorcamilodev.commerce.dto.UserDTO;
 import com.vitorcamilodev.commerce.entities.Role;
 import com.vitorcamilodev.commerce.entities.User;
 import com.vitorcamilodev.commerce.projections.UserDetailsProjection;
 import com.vitorcamilodev.commerce.repositories.UserRepository;
 
 @Service
-public class UserService implements UserDetailsService{
+public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
- 	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
-		if(result.size() == 0) {
+		if (result.size() == 0) {
 			throw new UsernameNotFoundException("User not found");
 		}
 		User user = new User();
 		user.setEmail(username);
 		user.setPassword(result.get(0).getPassword());
-		for(UserDetailsProjection projection : result) {
-			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));	
+		for (UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
-		
+
 		return user;
+	}
+
+	protected User authenticated() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
+
+			return repository.findByEmail(username).get();
+		} catch (Exception e) {
+			throw new UsernameNotFoundException("User not found");
+		}
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		return new UserDTO(authenticated());
 	}
 
 }
